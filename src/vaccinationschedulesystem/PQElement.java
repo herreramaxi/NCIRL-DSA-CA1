bf81e1598f33e0006c159961335afb887f594b03
Maxi <herreramaxi@gmail.com>
2021-03-08 00:13:09 +0000

    creating vaccination schedule system

---
+++ src/vaccinationschedulesystem/PQElement.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vaccinationschedulesystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Maximiliano Herrera
+ */
+public class PQElement {
+
+    private final Person element;
+    private final int key;
+
+    public Object getElement() {
+        return element;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public PQElement(Person element, int key) {
+        this.element = element;
+        this.key = key;
+    }
+
+    
+    //todo: check this
+    @Override
+    public String toString() {
+        return "PQElement{" + "element=" + element + ", key=" + key + '}';
+    }
+}
